c56e2293a31c339cb54985957a2258d6b781df47
simrantripathy <95369435+simrantripathy@users.noreply.github.com>
2022-04-02 23:47:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        baseUrl: "https://api.openweathermap.org/data/2.5/weather?q=s&appid=6af30c0ce65ee4a4f00701613cf96a9d",
+        key: "6af30c0ce65ee4a4f00701613cf96a9d"
+    }
+}
+
+module.exports = constants;
---
+++ utils/weatherData.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const constants = require('../config');
+
+const weatherData = (address, callback) => {
+    const url = constants.openWeatherMap.baseUrl + encodeURIComponent(address) + '&appid=' + constants.openWeatherMap.key;
+    console.log(url);
+    request({url, json:true}, (error, {body})=> {
+        if(error) {
+            callback("Can't fetch data from open weather map api ", undefined)
+        } else if(!body.main || !body.main.temp || !body.name || !body.weather) {
+            callback("Unable to find required data, try another location", undefined);
+        } else {
+            callback(undefined, {
+                temperature: body.main.temp,
+                description: body.weather[0].description,
+                cityName: body.name
+            })
+        }
+    })
+}
+
+module.exports = weatherData;
