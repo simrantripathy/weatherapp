0684c51156cfaafbe3ce2d91460549682995d41f
simrantripathy <95369435+simrantripathy@users.noreply.github.com>
2022-08-19 15:06:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
-        baseUrl: "https://api.openweathermap.org/data/2.5/weather?q=s&appid=6af30c0ce65ee4a4f00701613cf96a9d",
-        key: "6af30c0ce65ee4a4f00701613cf96a9d"
+        baseUrl: "https://api.openweathermap.org/data/2.5/weather?q=s&appid=",
+        key: 
     }
 }
 
